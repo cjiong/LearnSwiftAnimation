bb371a31ae7246ae91a62baef83019c21d86960e
cjiong <416137318@qq.com>
2016-05-25 00:26:43 +0800

    create Project 01 - TumblrMenu

---
+++ Project 01 - TumblrMenu/Project 01 - TumblrMenuTests/Project_01___TumblrMenuTests.swift
@@ -0,0 +1,36 @@
+//
+//  Project_01___TumblrMenuTests.swift
+//  Project 01 - TumblrMenuTests
+//
+//  Created by 陈炯 on 16/5/25.
+//  Copyright © 2016年 Jiong. All rights reserved.
+//
+
+import XCTest
+@testable import Project_01___TumblrMenu
+
+class Project_01___TumblrMenuTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
