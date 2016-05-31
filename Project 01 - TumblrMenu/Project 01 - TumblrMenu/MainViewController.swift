88ac39e8ee65c185eb1d05e593a55058a4a16aa5
cjiong <416137318@qq.com>
2016-05-31 22:44:57 +0800

    fixed bugs
    自动布局还得学啊

--- Project 01 - TumblrMenu/Project 01 - TumblrMenu/MainViewController.swift
+++ Project 01 - TumblrMenu/Project 01 - TumblrMenu/MainViewController.swift
@@ -12,6 +12,8 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.navigationBarHidden = true
+        
     }
 
     override func didReceiveMemoryWarning() {
