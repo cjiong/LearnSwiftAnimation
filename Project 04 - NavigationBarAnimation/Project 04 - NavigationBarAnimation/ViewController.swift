70ad5f1b795d4588702c55d30692c0ac14b460de
cjiong <416137318@qq.com>
2016-06-13 22:37:59 +0800

    fixed bug

--- Project 04 - NavigationBarAnimation/Project 04 - NavigationBarAnimation/ViewController.swift
+++ Project 04 - NavigationBarAnimation/Project 04 - NavigationBarAnimation/ViewController.swift
@@ -10,12 +10,12 @@ import UIKit
 
 class ViewController: UITableViewController {
     
-    var data = [ scenery(name: "", image: "4-1"),
-                 scenery(name: "", image: "4-2"),
-                 scenery(name: "", image: "4-3"),
-                 scenery(name: "", image: "4-4"),
-                 scenery(name: "", image: "4-5"),
-                 scenery(name: "", image: "4-6")
+    var data = [ scenery(name: "City", image: "4-1"),
+                 scenery(name: "Mountain", image: "4-2"),
+                 scenery(name: "Field", image: "4-3"),
+                 scenery(name: "Tree", image: "4-4"),
+                 scenery(name: "Snow", image: "4-5"),
+                 scenery(name: "Harbor", image: "4-6")
     
     ]
 
