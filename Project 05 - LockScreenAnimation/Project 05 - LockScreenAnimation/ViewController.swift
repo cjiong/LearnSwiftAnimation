7addfc0ba20bf2e7dbb54955163979125ac354de
cjiong <416137318@qq.com>
2016-06-17 18:54:44 +0800

    create Project 05 - LockScreenAnimation

---
+++ Project 05 - LockScreenAnimation/Project 05 - LockScreenAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project 05 - LockScreenAnimation
+//
+//  Created by 陈炯 on 16/6/16.
+//  Copyright © 2016年 Jiong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
