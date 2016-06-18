cef1e9dceb2b27c0af910656315b9e9a23737409
cjiong <416137318@qq.com>
2016-06-18 23:44:09 +0800

    complete Project 05 - LockScreenAnimation

--- Project 05 - LockScreenAnimation/Project 05 - LockScreenAnimation/ViewController.swift
+++ Project 05 - LockScreenAnimation/Project 05 - LockScreenAnimation/ViewController.swift
@@ -10,16 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var topView: UIImageView!
+    @IBOutlet weak var bottomView: UIImageView!
+    @IBOutlet weak var lockBorder: UIImageView!
+    @IBOutlet weak var lockKeyhole: UIButton!
+    
+    @IBAction func buttonDidTouch(sender: AnyObject) {
+        
+        openLock()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        UIApplication.sharedApplication().statusBarHidden = true
     }
-
-
+    
+    func openLock() {
+        
+        UIView.animateWithDuration(0.5, delay: 0.5, options: [], animations: {
+            
+            self.lockKeyhole.transform = CGAffineTransformMakeRotation(CGFloat(M_PI))
+            }, completion: {_ in
+                UIView.animateWithDuration(0.8, delay: 0.2, options: [], animations: {
+                    
+                    let deltaY = self.lockBorder.frame.maxY
+                    
+                    self.lockBorder.center.y -= deltaY
+                    self.lockKeyhole.center.y -= deltaY
+                    self.topView.center.y -= deltaY
+                    self.bottomView.center.y += deltaY
+                    
+                    }, completion:{ _ in
+                    
+                    self.topView.removeFromSuperview()
+                    self.bottomView.removeFromSuperview()
+                    self.lockBorder.removeFromSuperview()
+                    self.lockKeyhole.removeFromSuperview()
+                    })
+                
+        })
+    }
 }
 
