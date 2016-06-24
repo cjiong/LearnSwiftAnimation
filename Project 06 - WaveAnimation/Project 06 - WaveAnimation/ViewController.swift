226fac2c30c336da386cf9fab06049fdbf654e74
cjiong <416137318@qq.com>
2016-06-24 23:54:41 +0800

    complete Project 06 - WaveAnimation

--- Project 06 - WaveAnimation/Project 06 - WaveAnimation/ViewController.swift
+++ Project 06 - WaveAnimation/Project 06 - WaveAnimation/ViewController.swift
@@ -10,14 +10,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var circle1: UIView!
+    @IBOutlet weak var circle2: UIView!
+    @IBOutlet weak var circle3: UIView!
+    @IBOutlet weak var waveButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        circle1.layer.cornerRadius = 50
+        circle2.layer.cornerRadius = 50
+        circle3.layer.cornerRadius = 50
+        circle1.clipsToBounds = true
+        circle2.clipsToBounds = true
+        circle3.clipsToBounds = true
     }
 
+    @IBAction func waveButtonDidTapped(sender: AnyObject) {
+        
+        UIView.animateWithDuration(3.0, delay: 0.0, options: [.CurveEaseInOut, .Repeat], animations: {
+            
+            self.circle3.transform = CGAffineTransformMakeScale(8.0, 8.0)
+            self.circle3.alpha = 0.0
+            
+            }, completion: { _ in
+                
+                self.circle3.transform = CGAffineTransformIdentity
+                self.circle3.alpha = 1.0
+        })
+        
+        UIView.animateWithDuration(3.0, delay: 0.3, options: [.CurveEaseInOut, .Repeat], animations: {
+            
+            self.circle2.transform = CGAffineTransformMakeScale(8.0, 8.0)
+            self.circle2.alpha = 0.0
+            
+            }, completion: { _ in
+                
+                self.circle2.transform = CGAffineTransformIdentity
+                self.circle2.alpha = 1.0
+        })
+        
+        UIView.animateWithDuration(3.0, delay: 0.6, options: [.CurveEaseInOut, .Repeat], animations: {
+            
+            self.circle1.transform = CGAffineTransformMakeScale(8.0, 8.0)
+            self.circle1.alpha = 0.0
+            
+            }, completion: { _ in
+                
+                self.circle1.transform = CGAffineTransformIdentity
+                self.circle1.alpha = 1.0
+        })
+
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
