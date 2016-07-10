85c3d98d5ef2e3a85e42d6a9f34facb870789060
cjiong <416137318@qq.com>
2016-07-10 18:16:23 +0800

    complete Project 07 - GradientAnimation

--- Project 07 - GradientAnimation/Project 07 - GradientAnimation/ViewController.swift
+++ Project 07 - GradientAnimation/Project 07 - GradientAnimation/ViewController.swift
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+
 
 }
 
