32a05d03ec8535fce05ea1ec33add9bb578dc17c
cjiong <416137318@qq.com>
2016-08-26 13:24:32 +0800

    complete Project 14 - ElasticDrawer

---
+++ Project 14 - ElasticDrawer/Project 14 - ElasticDrawer/DrawerCell.swift
@@ -0,0 +1,39 @@
+//
+//  DrawerCell.swift
+//  Project 14 - ElasticDrawer
+//
+//  Created by 陈炯 on 16/8/26.
+//  Copyright © 2016年 Jiong. All rights reserved.
+//
+
+import UIKit
+
+struct Player {
+    
+    let name: String
+    let pic: String
+    
+    init(name: String, pic: String) {
+        
+        self.name = name
+        self.pic = pic
+    }
+}
+
+class DrawerCell: UITableViewCell {
+    
+    @IBOutlet weak var playerImageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+    }
+    
+}
---
+++ Project 14 - ElasticDrawer/Project 14 - ElasticDrawer/DrawerTableViewController.swift
@@ -0,0 +1,54 @@
+//
+//  DrawerTableViewController.swift
+//  Project 14 - ElasticDrawer
+//
+//  Created by 陈炯 on 16/8/26.
+//  Copyright © 2016年 Jiong. All rights reserved.
+//
+
+import UIKit
+
+class DrawerTableViewController: UITableViewController {
+    
+    let data = [
+        Player(name: "Tracy McGrady", pic: "1"),
+        Player(name: "Kobe Bryant", pic: "2"),
+        Player(name: "Michael Jordan", pic: "3"),
+        Player(name: "LeBron James", pic: "4"),
+        Player(name: "Dwight Howard", pic: "5"),
+        Player(name: "Jeremy Lin", pic: "6"),
+        Player(name: "Kevin Durant", pic: "7"),
+        Player(name: "Stephen Curry", pic: "8")
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    // MARK: - Table view data source
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell") as! DrawerCell
+        
+        cell.nameLabel.text = data[indexPath.row].name
+        cell.imageView?.image = UIImage(named: data[indexPath.row].pic)
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 80
+    }
+}
