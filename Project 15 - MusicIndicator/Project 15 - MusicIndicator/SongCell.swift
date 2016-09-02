1919f732e549b4d8ef5c3e998bacf59d1352c0ca
cjiong <416137318@qq.com>
2016-09-02 22:03:40 +0800

    complete Project 15 - MusicIndicator

---
+++ Project 15 - MusicIndicator/Project 15 - MusicIndicator/SongCell.swift
@@ -0,0 +1,35 @@
+//
+//  SongCell.swift
+//  Project 15 - MusicIndicator
+//
+//  Created by 陈炯 on 16/9/2.
+//  Copyright © 2016年 Jiong. All rights reserved.
+//
+
+import UIKit
+
+struct Song {
+    
+    let name: String
+    let artist: String
+    init(name: String, artist: String) {
+        self.name = name
+        self.artist = artist
+    }
+}
+
+class SongCell: UITableViewCell {
+    
+    @IBOutlet weak var namaeLabel: UILabel!
+    @IBOutlet weak var artistLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+    }
+    
+}
